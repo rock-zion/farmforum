51d230b6dabb2a073972523d3100bc5f2052d652
rockxzion <102598736+rockxzion@users.noreply.github.com>
2024-01-27 22:30:11 +0100

    define controllers for question crud

--- controllers/questions.go
+++ controllers/questions.go
@@ -1,3 +1,21 @@
 package controllers
 
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 // this file houses all crud logic for questions
+
+func AddQuestion(farmforumDatabase *mongo.Database) http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+func UpdateQuestion(farmforumDatabase *mongo.Database) http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+func FetchQuestionById() http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
