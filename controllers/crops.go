320fa48d85daefcc6ad408511f1a919230a22c57
rockxzion <102598736+rockxzion@users.noreply.github.com>
2024-01-27 18:19:33 +0100

    write logic to fetch all crops

--- controllers/crops.go
+++ controllers/crops.go
@@ -8,13 +8,27 @@ import (
 
 	"github.com/farmforum/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // this file houses all crud logic for questions
 
 // fetch all crops
 func FetchAllCrops(farmforumDatabase *mongo.Database) http.Handler {
-	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+	cropCollection := farmforumDatabase.Collection("crop")
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {
+		cursor, err := cropCollection.Find(context.TODO(), bson.M{})
+		if err != nil {
+			panic(err)
+		}
+		var crops []models.Crop
+		if err = cursor.All(context.TODO(), &crops); err != nil {
+			panic(err)
+		}
+		w.Header().Set("Content-Type", "application/vnd.api+json")
+		json.NewEncoder(w).Encode(crops)
+
+	}))
 }
 
 // add a crop entry
