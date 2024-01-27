4829608eb0e64d69bffaf0f763dcd2a1dc7e21ac
rockxzion <102598736+rockxzion@users.noreply.github.com>
2024-01-27 17:59:21 +0100

    define controllers for crops' crud operations

--- controllers/crops.go
+++ controllers/crops.go
@@ -1,3 +1,34 @@
 package controllers
 
-// this file houses all crud logic for crops
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// this file houses all crud logic for questions
+
+// fetch all crops
+func FetchAllCrops(famrmforumDatabase *mongo.Database) http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+// add a crop entry
+func AddCrop(famrmforumDatabase *mongo.Database) http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+// delete a crop entry
+func DeleteCrop(famrmforumDatabase *mongo.Database) http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+// edit a crop entry
+func EditCrop(famrmforumDatabase *mongo.Database) http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
+
+// insert many a crop entry
+func InsertManyCrops(famrmforumDatabase *mongo.Database) http.Handler {
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {}))
+}
