0362f9a93654179ec924ae956e29cd800fb267d5
rockxzion <102598736+rockxzion@users.noreply.github.com>
2024-01-31 21:03:08 +0100

    write logic to add user to users collection

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,14 @@
 package controllers
 
 import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	// "github.com/farmforum/controllers"
+	"github.com/farmforum/models"
+	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -13,3 +21,33 @@ func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
+
+func HandleAddUser(db *mongo.Database) http.Handler {
+	userCollection := db.Collection("users")
+	var userDocument models.User
+	return http.HandlerFunc((func(w http.ResponseWriter, r *http.Request) {
+
+		err := json.NewDecoder(r.Body).Decode(&userDocument)
+		if err != nil {
+			panic(err)
+		}
+
+		currentTime := time.Now()
+		userDocument.CreatedAt = currentTime
+		userDocument.UpdatedAt = currentTime
+		hashedPassword, err := HashPassword(userDocument.Password)
+		if err != nil {
+			panic(err)
+		}
+		userDocument.Password = hashedPassword
+
+		_, err = userCollection.InsertOne(context.TODO(), userDocument)
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+
+	}))
+}
+
