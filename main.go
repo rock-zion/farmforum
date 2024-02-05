42ac0d959bdf60184f83b7a51df9311f97780521
rockxzion <102598736+rockxzion@users.noreply.github.com>
2024-02-05 23:43:46 +0100

    refactor routes declaration

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/farmforum/controllers"
 	"github.com/gorilla/mux"
+	"github.com/farmforum/routes"
 	"github.com/joho/godotenv"
 )
 
@@ -40,26 +41,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	router := mux.NewRouter()
-	router.Handle("/crops", controllers.AddCrop(famrmforumDatabase)).Methods("POST")
-	router.Handle("/crops", controllers.FetchAllCrops(famrmforumDatabase)).Methods("GET")
-	router.Handle("/crops/{id}", controllers.FetchCropById(famrmforumDatabase)).Methods("GET")
-	router.Handle("/crops/many", controllers.InsertManyCrops(famrmforumDatabase)).Methods("POST")
-	router.Handle("/crops/{id}", controllers.DeleteCrop(famrmforumDatabase)).Methods("DELETE")
-	router.Handle("/crops/{id}", controllers.EditCrop(famrmforumDatabase)).Methods("PUT")
-	router.Handle("/crops/questions/{id}", controllers.FetchQuestionByCropId(famrmforumDatabase)).Methods("GET")
-
-	//
-	router.Handle("/question/{id}", controllers.AddQuestion(famrmforumDatabase)).Methods("POST")
-	router.Handle("/questions/{id}", controllers.UpdateQuestion(famrmforumDatabase)).Methods("GET")
-
-	// routers to handle user actions
-	router.Handle("/user", controllers.HandleAddUser(famrmforumDatabase)).Methods(("POST"))
-	router.Handle("/user/login", controllers.HandleLogUserIn(famrmforumDatabase)).Methods(("POST"))
+}
 
 	server := &http.Server{
 		Addr:    os.Getenv("SERVER_ADDR"),
-		Handler: router,
+		Handler: routes.Routes(),
 	}
 
 	server.ListenAndServe()
